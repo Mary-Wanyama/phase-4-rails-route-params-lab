3e02eb91bdb08b05b8442fad588315ebe7fd61f3
Mary-Wanyama <mary.wanyama@student.moringaschool.com>
2022-11-24 08:36:05 +0300

    completed assignment

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,14 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+    def show 
+      student = Student.find(params[:id])
+      render json: student
+    end
+
+    def display 
+      # students = Student.find_by(params[:name])
+      # render json: students
+      
+    end
 end
